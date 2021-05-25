3ce027361fca324a56abffe1215d2ff1ddd991eb
Kruore <33648811+kruore@users.noreply.github.com>
2021-05-25 16:07:49 +0900

    0525_First Make

---
+++ KINLAB_HealthPlus/Assets/GM_PanelManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM_PanelManager : MonoBehaviour
+{
+    [Header("Panel")]
+    [Space(5f)]
+    public GameObject StartPanel;
+    public GameObject SetInformationPanel;
+    public GameObject FiveSecPassed_Panel;
+    public GameObject ApplicationMainPanel;
+    public GameObject TodayCheckPanel;
+    public GameObject TodayCheck_2_Panel;
+    public GameObject EattingTypePanel;
+    public GameObject EAT_3Sec_Pass_Panel;
+
+}
